9585c5af631697b1a9f243530860be0aa09df4f9
Max Milton <max@wearegenki.com>
2016-01-11 07:38:06 +1100

    Add support for jshint linting

--- themes/wearegenki/static/js/init.js
+++ themes/wearegenki/static/js/init.js
@@ -2,31 +2,31 @@
 // TODO: If url is the same, don't animate
 
 $(function(){
-  'use strict';
-  var options = {
+    'use strict';
+    var options = {
         prefetch: true,
         cacheLength: 16,
         onStart: {
-          duration: 150, // Duration of our animation
-          render: function ($container) {
-            // Add your CSS animation reversing class
-            $container.addClass('is-exiting');
+            duration: 150, // Duration of our animation
+            render: function ($container) {
+                // Add your CSS animation reversing class
+                $container.addClass('is-exiting');
 
-            // Restart your animation
-            smoothState.restartCSSAnimations();
-          }
+                // Restart your animation
+                smoothState.restartCSSAnimations();
+            }
         },
         onReady: {
-          duration: 0,
-          render: function ($container, $newContent) {
-            // Remove your CSS animation reversing class
-            $container.removeClass('is-exiting');
+            duration: 0,
+            render: function ($container, $newContent) {
+                // Remove your CSS animation reversing class
+                $container.removeClass('is-exiting');
 
-            // Inject the new content
-            $container.html($newContent);
+                // Inject the new content
+                $container.html($newContent);
 
-          }
+            }
         }
-      },
-      smoothState = $('#main').smoothState(options).data('smoothState');
+    },
+    smoothState = $('#main').smoothState(options).data('smoothState');
 });
